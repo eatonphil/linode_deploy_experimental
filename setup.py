f917c015724982632bce7b1a5367dc868e875844
Phil Eaton <peaton@linode.com>
2017-06-25 13:40:40 -0400

    Add ssh.tcl to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup
 
 setup(name='linode-deploy-experimental',
-      version='0.2',
+      version='0.3',
       description='Deploy experimental disk images to Linode',
       author='Phil Eaton',
       url='https://github.com/eatonphil/linode-deploy-experimental',
       install_requires=['linode_api3'],
       packages=['linode_deploy_experimental'],
+      data_files=[('linode_deploy_experimental', ['ssh.tcl'])],
       entry_points={
           'console_scripts': ['linode_deploy_experimental = linode_deploy_experimental.__main__:main'],
       },
